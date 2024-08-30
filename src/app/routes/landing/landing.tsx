433926e6e2674a81159ca8c8d6f4ffafb152903e
josup <josupod@gmail.com>
2024-08-30 14:47:37 +0200

    Basic landing and styling.

--- src/app/routes/landing/landing.tsx
+++ src/app/routes/landing/landing.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import styles from "./landing.module.css";
+import styles from "./landing.module.scss";
 
 const LandingRoute = () => {
-    return <div>LandingPage</div>;
+    return (
+        <div className={styles.root}>
+            <div className={styles.content}>
+                <h1>
+                    Welcome to <b>e</b>atly!
+                </h1>
+                <p>This is a simple landing page for the app.</p>
+                <div className={styles.buttonContainer}>
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LandingRoute;

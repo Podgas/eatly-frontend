761de8deeaf5a85a5674be6d646011f8f0ebe303
josup <josupod@gmail.com>
2024-08-31 12:18:51 +0200

    path aliases update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    base: "./",
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+        port: 5173,
+    },
+    preview: {
+        port: 5173,
+    },
+    optimizeDeps: { exclude: ["fsevents"] },
+    build: {
+        rollupOptions: {
+            external: ["fs/promises"],
+            output: {
+                experimentalMinChunkSize: 3500,
+            },
+        },
+    },
     css: {
         preprocessorOptions: {
             scss: {

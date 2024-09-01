0e0f58a96c9f3a08ae7800bde4834c27717344e1
josup <josupod@gmail.com>
2024-09-01 14:02:58 +0200

    textField modification.
    -Now the component don't handle the value and is handled outside
    -Helper text added
    -Error helper text handled
    -Added name and type for propper form functionality

--- src/components/textField/textField.tsx
+++ src/components/textField/textField.tsx
@@ -1,41 +1,45 @@
-import React, { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import styles from "./textField.module.scss";
 import { generateClassNames } from "@/utils/componentUtils";
 
 interface TextFieldProps {
+    name: string;
     label: string;
+    inputType?: "text" | "password";
     variant?: "standard" | "outlined" | "filled";
     value?: string;
     size?: "sm" | "lg";
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
     disabled?: boolean;
     error?: boolean;
+    helperText?: string;
 }
 
 export const TextField = ({ ...props }: TextFieldProps) => {
     const className = generateClassNames(props, styles["input"], styles);
-
-    //State hooks
-    const [inputValue, setInputValue] = useState("");
-
-    const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value);
-
-        setInputValue(event.target.value);
-    };
+    const helperTextClassName = generateClassNames(
+        { error: props.error },
+        styles["helperText"],
+        styles
+    );
 
     return (
         <div className={styles.fieldRoot}>
             <div className={styles.inputContainer}>
                 <input
                     className={className}
-                    value={inputValue}
-                    onChange={handleValueChange}
+                    value={props.value}
+                    onChange={props.onChange}
                     disabled={props.disabled}
                     placeholder=" "
+                    name={props.name}
+                    type={props.inputType}
                 />
                 <label className={styles.label}>{props.label}</label>
             </div>
+            {props.error && (
+                <span className={helperTextClassName}>{props.helperText}</span>
+            )}
         </div>
     );
 };

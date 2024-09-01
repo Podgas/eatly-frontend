5ff309b4d4328c0864a1ca1c2d0635347820536d
josup <josupod@gmail.com>
2024-09-01 14:04:55 +0200

    First iteration on the register form using React-hook-form Added styling

--- src/app/routes/auth/register/register.tsx
+++ src/app/routes/auth/register/register.tsx
@@ -1,17 +1,33 @@
 "use client";
-import { useForm } from "react-hook-form";
 import styles from "./register.module.scss";
+import { Button } from "@/components/button";
+import { Logo } from "@/components/logo";
+import { FormInputText } from "@/components/form-inputs";
+import { FormProvider, useForm, SubmitHandler } from "react-hook-form";
+
+import { DevTool } from "@hookform/devtools";
+
+type FormValues = {
+    username: string;
+    email: string;
+};
 
 export const RegisterRoute = () => {
+    const methods = useForm<FormValues>();
     const {
+        control,
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm();
+    } = methods;
 
-    const onSubmit = handleSubmit(async (data) => {
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log("submitted", data);
+    };
+
+    /*const onSubmit2 = handleSubmit(async (data) => {
         console.log(JSON.stringify(data));
-        const res = await fetch("http://localhost:3000/auth/register", {
+        const res = await fetch("http://localhost:3000/auth/a", {
             method: "POST",
             body: JSON.stringify(data),
             headers: {
@@ -20,45 +36,48 @@ export const RegisterRoute = () => {
         });
         const resJSON = await res.json();
         console.log(resJSON);
-    });
+    });*/
 
     return (
-        <div className={styles["form-container"]}>
-            <form onSubmit={onSubmit} className={styles["form"]}>
-                <input
-                    type="text"
-                    {...register("username", {
-                        required: "Username is required",
-                    })}
-                    placeholder="Username"
-                />
-                {errors.username && (
-                    <span className={styles["error"]}>
-                        {errors.username.message as string}
-                    </span>
-                )}
-                <input
-                    type="email"
-                    {...register("email", { required: "Email is required" })}
-                    placeholder="Email"
-                />
-                <input
-                    type="password"
-                    {...register("password", {
-                        required: "Password is required",
-                    })}
-                    placeholder="Password"
-                />
-                <input
-                    type="password"
-                    {...register("confirmPassword", {
-                        required: "Confirm Password is required",
-                    })}
-                    placeholder="Confirm Password"
-                />
+        <div className={styles.root}>
+            <div className={styles.content}>
+                <div className={styles.logoContainer}>
+                    <Logo />
+                </div>
+                <div className={styles.textContainer}>
+                    <h5>Create an account</h5>
+                    <p>Setup a new account in a minute. </p>
 
-                <button>Register</button>
-            </form>
+                    <FormProvider {...methods}>
+                        <form
+                            className={styles.form}
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            <FormInputText label="Name" name="name" req />
+                            <FormInputText label="Email" name="email" req />
+                            <FormInputText
+                                label="Password"
+                                name="password"
+                                req
+                                type="password"
+                            />
+                            <FormInputText
+                                label="Repeat Password"
+                                name="password_repeat"
+                                req
+                                type="password"
+                            />
+                            <Button
+                                label="Register"
+                                variant="primary"
+                                size="lg"
+                                type="submit"
+                            />
+                        </form>
+                        <DevTool control={control} />
+                    </FormProvider>
+                </div>
+            </div>
         </div>
     );
 };

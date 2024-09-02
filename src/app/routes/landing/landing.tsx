208c3f168dfabed2d70a3b421ae35fc61bbdbf2c
josup <josupod@gmail.com>
2024-09-02 15:41:00 +0200

    Minor change in landing

--- src/app/routes/landing/landing.tsx
+++ src/app/routes/landing/landing.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./landing.module.scss";
-import { Button } from "@/components/button";
+import { Button } from "@/components/UI/button";
 import { Link } from "react-router-dom";
 
 const LandingRoute = () => {
     return (
         <div className={styles.root}>
             <div className={styles.content}>
-                <h1>
+                <h4>
                     Welcome to <b>e</b>atly!
-                </h1>
+                </h4>
                 <p>This is a simple landing page for the app.</p>
                 <div className={styles.buttonContainer}>
                     <Link to="/auth/login">
